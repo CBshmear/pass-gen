07c9dcdf1255aa77dae588d7870abf9d8159e0da
unknown <connorbeer13@gmail.com>
2022-11-07 16:47:44 -0600

    Added prompts confirming choices

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var specials = "!@#$%^&*()";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var lowerCases = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 
@@ -17,10 +17,36 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword () {
+// Prompts obtaining and confirming password specs, after you press generate button
+function generatePassword () { 
   var passwordLength = window.prompt ("Please enter the number of characters you want. It must be no less than 8 and no greater than 128");
+  window.alert("Your password will be " + passwordLength + " characters long.");
+
   var numbers = window.confirm ("Do you want numbers in your password?");
-  var lowerCases = window.confirm("Do you want lower case letters in your password?");
-  var upperCases = window.confirm("Do you wants upper case letters in your password?");
-  var specials = window.confirm("Do you want special characters in your password?");
+  if(numbers === true) {
+    window.alert("Your password will contain numbers.");
+   }else  {
+    window.alert("Your password will not contain numbers.");
+   } 
+
+   var lowerCases = window.confirm("Do you want lower case letters in your password?");
+   if (lowerCases === true) {
+    window.alert("Your password will contain lowercase letters.");
+   }else{
+    window.alert("Your password will not contain lowercase letters.");
+   }
+
+   var upperCases = window.confirm("Do you wants upper case letters in your password?");
+   if (upperCases === true) {
+    window.alert("Your password will contain uppercase letters");
+   }else{
+   window.alert("Your password will not contain uppercase letters.");
+   }
+
+   var specials = window.confirm("Do you want special characters in your password?");
+  if (specials === true) {
+  window.alert("Your password will contain special characters.");
+  }else{
+  window.alert("Your password will not contain special characters.");
+  }
 }
