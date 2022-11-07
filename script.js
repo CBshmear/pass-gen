aa53d1c488fc034c350607866e9f9454f098f415
unknown <connorbeer13@gmail.com>
2022-11-06 18:50:12 -0600

    added vars and promps to script

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specials = "!@#$%^&*()";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +11,16 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword () {
+  var passwordLength = window.prompt ("Please enter the number of characters you want. It must be no less than 8 and no greater than 128");
+  var numbers = window.confirm ("Do you want numbers in your password?");
+  var lowerCases = window.confirm("Do you want lower case letters in your password?");
+  var upperCases = window.confirm("Do you wants upper case letters in your password?");
+  var specials = window.confirm("Do you want special characters in your password?");
+}
